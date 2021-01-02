e1c6988720cde5624026e8b5d7d97bef5c40b242
Ariq Fachry R <61013150+AriqF@users.noreply.github.com>
2021-01-03 00:03:01 +0700

    Update hiraganaQuiz1.php

--- hiraganaQuiz1.php
+++ hiraganaQuiz1.php
@@ -125,8 +125,4 @@
     <script src="src/quizQuestion.js"></script>
     <script src="src/app.js"></script>
     </body>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
-
-
 </html>
