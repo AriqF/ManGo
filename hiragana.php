03db6f9b2d084ba9d44e686483e6616a04753ac5
Ariq Fachry R <61013150+AriqF@users.noreply.github.com>
2021-01-03 00:13:18 +0700

    Update hiragana.php

--- hiragana.php
+++ hiragana.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>  </title>
+        <title>Man-Go | Belajar Hiragana</title>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
